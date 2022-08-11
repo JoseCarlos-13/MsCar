9fd748b3bfb24d6534965f9e35b083df0eb38525
José Carlos <josecarlosbsilva.17@gmail.com>
2022-08-11 17:36:45 -0300

    work in progress

--- index.js
+++ index.js
@@ -7,6 +7,6 @@ express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('src/HTML/mainpage'))
+  .get('/', (req, res) => res.render('pages/index'))
   .get('/cool', (req, res) => res.send(cool()))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`));
